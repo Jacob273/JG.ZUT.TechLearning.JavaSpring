ca2b494a4c074c0c7ca67113ce764d625420dc0e
jacob273 <jakubgmur@op.pl>
2020-12-13 10:59:33 +0100

    Instantiated context Added @Configuration annotation on configuration class Added ContextConfiguration and @RunWith on test class

--- lab02/stereo1/src/main/java/soundsystem/CDPlayerMain.java
+++ lab02/stereo1/src/main/java/soundsystem/CDPlayerMain.java
@@ -1,8 +1,17 @@
 package soundsystem;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class CDPlayerMain {
 
 	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(
+				CDPlayerConfig.class);
+		
+		context.close();
+		
 		CompactDisc cd = new SgtPeppers();
 		MediaPlayer player = new CDPlayer(cd);
 		player.play();
--- lab02/stereo1/src/test/java/soundsystem/CDPlayerTest.java
+++ lab02/stereo1/src/test/java/soundsystem/CDPlayerTest.java
@@ -7,8 +7,12 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+@ContextConfiguration(classes=CDPlayerConfig.class)
+@RunWith(SpringJUnit4ClassRunner.class) 
 public class CDPlayerTest {
 
   @Rule
